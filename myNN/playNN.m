24902d777de93628c5d16bfaefdd0c7ec672f57a
Feichao Qian <feqian@UCSD.EDU>
2016-01-24 23:12:46 -0800

    add figures

--- myNN/playNN.m
+++ myNN/playNN.m
@@ -18,22 +18,23 @@ function playNN(dataFile)
     % build neural networks
     rand('state', 0); %Setting the generator to the same fixed state allows computations to be repeated.
     nn = buildNN([784, 100, 10]);
-%==========================================================================
-    %Ex(d) Classification on MNIST database using vanilla neural networks 
-    %set up the parameters of experiments
-    exp.numEpochs = 1; 
-    exp.batchSize = 100;
-    exp.valid = 1; % cross-validation to decide the stopping criteria
-    nn = trainNN(nn, train_x, train_y, exp, test_x, test_y);
-    plot([1:exp.numEpochs],nn.trainAcc,[1:exp.numEpochs],nn.validAcc, [1:exp.numEpochs], nn.testAcc);
-    legend('Training set', 'Validation Set', 'Test Set');
-    %check numerical gradient 
-    %nn = compGrad(nn, train_x(1:100, :), train_y(1:100, :));
-    acc = testNN(nn, test_x, test_y);
+% %==========================================================================
+%     %Ex(d) Classification on MNIST database using vanilla neural networks 
+%     %set up the parameters of experiments
+%     exp.numEpochs = 20; 
+%     exp.batchSize = 100;
+%     exp.valid = 1; % cross-validation to decide the stopping criteria
+%     nn = trainNN(nn, train_x, train_y, exp, test_x, test_y);
+%     plot([1:exp.numEpochs],nn.trainAcc,[1:exp.numEpochs],nn.validAcc, [1:exp.numEpochs], nn.testAcc);
+%     legend('Training set', 'Validation Set', 'Test Set');
+%     %check numerical gradient 
+%     %nn = compGrad(nn, train_x(1:100, :), train_y(1:100, :));
+%     acc = testNN(nn, test_x, test_y);
 % %==========================================================================
 %     %Ex(e) Experiment with Regularization(L2 w = w - alpha (dEdW + lambda w))
 %     nn.lambda = 0.0001;
-%     exp.numEpochs = 1;
+%     exp.numEpochs = 20;
+%     exp.batchSize = 100;
 %     exp.valid = 0;
 %     nn = trainNN(nn, train_x, train_y, exp, test_x, test_y);
 %     plot([1:exp.numEpochs],nn.trainAcc, [1:exp.numEpochs], nn.testAcc);
@@ -42,30 +43,63 @@ function playNN(dataFile)
 % %==========================================================================    
 %     %Ex(f) Experiment with Momentum
 %     nn.gamma = 0.9;
-%     exp.numEpochs = 1;
+%     nn.lambda = 0.0001;
+%     exp.numEpochs = 20;
+%     exp.batchSize = 100;
+%     exp.valid = 0;
 %     nn = trainNN(nn, train_x, train_y, exp, test_x, test_y);
 %     plot([1:exp.numEpochs],nn.trainAcc, [1:exp.numEpochs], nn.testAcc);
 %     legend('Training set', 'Test Set');
 %     acc = testNN(nn, test_x, test_y);
+% %==========================================================================
+%     %Ex(g) Experiment with different Activations--Funny Tanh
+%     nn.activeFunc = 'Tanh';
+%     exp.numEpochs = 20;
+%     exp.batchSize = 100;
+%     exp.valid = 0;
+%     nn = trainNN(nn, train_x, train_y, exp, test_x, test_y);
+%     plot([1:exp.numEpochs],nn.trainAcc, [1:exp.numEpochs], nn.testAcc);
+%     legend('Training set', 'Test Set');
+%     acc = testNN(nn, test_x, test_y);
+% %==========================================================================
+%     %Ex(g) Experiment with different Activations--ReLU
+%     nn.activeFunc = 'ReLU';
+%     exp.numEpochs = 100;
+%     exp.batchSize = 100;
+%     exp.valid = 0;
+%     nn = trainNN(nn, train_x, train_y, exp, test_x, test_y);
+%     plot([1:exp.numEpochs],nn.trainAcc, [1:exp.numEpochs], nn.testAcc);
+%     legend('Training set', 'Test Set');
+%     acc = testNN(nn, test_x, test_y);
+% %==========================================================================
+%     %Ex(h) Experiemnt with different Network Topology, 50 hidden 
+%     nn = buildNN([784, 50, 10]);
+%     exp.numEpochs = 20; 
+%     exp.batchSize = 100;
+%     exp.valid = 1; % cross-validation to decide the stopping criteria
+%     nn = trainNN(nn, train_x, train_y, exp, test_x, test_y);
+%     plot([1:exp.numEpochs],nn.trainAcc,[1:exp.numEpochs],nn.validAcc, [1:exp.numEpochs], nn.testAcc);
+%     legend('Training set', 'Validation Set', 'Test Set');
+%     acc = testNN(nn, test_x, test_y);
+% %==========================================================================
+%     %Ex(h) Experiemnt with different Network Topology, 200 hidden
+%     nn = buildNN([784, 200, 10]);
+%     exp.numEpochs = 20; 
+%     exp.batchSize = 100;
+%     exp.valid = 1; % cross-validation to decide the stopping criteria
+%     nn = trainNN(nn, train_x, train_y, exp, test_x, test_y);
+%     plot([1:exp.numEpochs],nn.trainAcc,[1:exp.numEpochs],nn.validAcc, [1:exp.numEpochs], nn.testAcc);
+%     legend('Training set', 'Validation Set', 'Test Set');
+%     acc = testNN(nn, test_x, test_y);
 %==========================================================================
-    %Ex(g) Experiment with different Activations--Funny Tanh
-    nn.activeFunc = 'Tanh'
-    exp.numEpochs = 20;
+    %Ex(h) Experiemnt with different Network Topology, 2 hidden layer
+    nn = buildNN([784, 100, 100, 10]);
+    exp.numEpochs = 20; 
     exp.batchSize = 100;
-    exp.valid = 0;
+    exp.valid = 1; % cross-validation to decide the stopping criteria
     nn = trainNN(nn, train_x, train_y, exp, test_x, test_y);
-    plot([1:exp.numEpochs],nn.trainAcc, [1:exp.numEpochs], nn.testAcc);
-    legend('Training set', 'Test Set');
+    plot([1:exp.numEpochs],nn.trainAcc,[1:exp.numEpochs],nn.validAcc, [1:exp.numEpochs], nn.testAcc);
+    legend('Training set', 'Validation Set', 'Test Set');
     acc = testNN(nn, test_x, test_y);
-%==========================================================================
-    %Ex(g) Experiment with different Activations--ReLU
-    nn.activeFunc = 'ReLU'
-    exp.numEpochs = 20;
-    nn = trainNN(nn, train_x, train_y, exp, test_x, test_y);
-    plot([1:exp.numEpochs],nn.trainAcc, [1:exp.numEpochs], nn.testAcc);
-    legend('Training set', 'Test Set');
-    acc = testNN(nn, test_x, test_y);
-%==========================================================================
-    %Ex(h) Experiemnt with different Network Topology
 end
 
